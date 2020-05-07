d5c65bbfa690bc3e770549620f21660f0774761c
Reece Bryant <reececb@gmail.com>
2020-05-07 14:05:07 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from "./store"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App store={store} />,
   document.getElementById('root')
 );
 
